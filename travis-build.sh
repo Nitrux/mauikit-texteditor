c548235e84ee18bcae59373880994b7ed94cafd1
Uri Herrera <hello@uriherrera.com>
2021-08-09 22:20:09 -0500

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -28,9 +28,9 @@ mk-build-deps -i -t "apt-get --yes" -r
 
 git clone --single-branch --branch v2.0 https://invent.kde.org/maui/mauikit-filebrowsing.git
 
-mv mauikit-imagetools/* .
+mv mauikit-texteditor/* .
 
-rm -rf mauikit-imagetools examples LICENSES README.md
+rm -rf mauikit-texteditor examples LICENSES README.md
 
 ### Build Deb
 

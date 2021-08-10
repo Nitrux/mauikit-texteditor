cdab1bb483570959ad30ee5ea7a63531d9e7a3cb
Uri Herrera <hello@uriherrera.com>
2021-08-10 15:21:06 -0500

    rebuild using ppa

--- travis-build.sh
+++ travis-build.sh
@@ -5,12 +5,16 @@ set -x
 apt -qq update
 apt -qq -yy install equivs curl git wget gnupg2
 
+wget -qO /etc/apt/sources.list.d/kubuntu-backports-ppa.list https://raw.githubusercontent.com/Nitrux/iso-tool/legacy/configs/files/sources.list.backports.ppa
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+	2836CB0A8AC93F7A > /dev/null
+
 curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -;
 
 wget -qO /etc/apt/sources.list.d/nitrux-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux
 
 apt -qq update
-
 ### Install Dependencies
 
 DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules appstream qml-module-qtquick-controls2 qml-module-qtquick-shapes qml-module-qtgraphicaleffects mauikit-dev libkf5syntaxhighlighting-dev

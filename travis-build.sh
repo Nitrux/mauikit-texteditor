bec167fd09b288af1c12caea9d1c1c603e9df557
Uri Herrera <hello@uriherrera.com>
2021-08-09 22:43:50 -0500

    add pkg

--- travis-build.sh
+++ travis-build.sh
@@ -20,7 +20,7 @@ apt -qq update
 
 ### Install Dependencies
 
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules appstream qml-module-qtquick-controls2 qml-module-qtquick-shapes qml-module-qtgraphicaleffects mauikit-dev
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules appstream qml-module-qtquick-controls2 qml-module-qtquick-shapes qml-module-qtgraphicaleffects mauikit-dev libkf5syntaxhighlighting-dev
 
 mk-build-deps -i -t "apt-get --yes" -r
 

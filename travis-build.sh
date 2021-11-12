5629554bc8b0c2f61d661506a2d2d67b62c4325f
Uri Herrera <hello@uriherrera.com>
2021-11-11 22:22:12 -0600

    screw debhelper, use checkinstall

--- travis-build.sh
+++ travis-build.sh
@@ -2,39 +2,111 @@
 
 set -x
 
-apt -qq update
-apt -qq -yy install equivs curl git wget gnupg2
+### Install Build Tools #1
 
-### FIXME - the container mauikit/ubuntu-18.04-amd64 does have the neon repo but for some idiotic reason it isn't working here
+DEBIAN_FRONTEND=noninteractive apt -qq update
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
+	appstream \
+	automake \
+	autotools-dev \
+	build-essential \
+	checkinstall \
+	cmake \
+	curl \
+	devscripts \
+	equivs \
+	extra-cmake-modules \
+	gettext \
+	git \
+	gnupg2 \
+	lintian \
+	wget
+
+### Add Neon Sources
 
 wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
 	55751E5D > /dev/null
 
-curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -;
+curl -L https://packagecloud.io/nitrux/testing/gpgkey | apt-key add -;
 
-wget -qO /etc/apt/sources.list.d/nitrux-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux
+wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux.testing
 
-apt -qq update
+DEBIAN_FRONTEND=noninteractive apt -qq update
 
-### Install Dependencies
+### Install Package Build Dependencies #2
+### Texteditor needs ECM > 5.70
 
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules appstream qml-module-qtquick-controls2 qml-module-qtquick-shapes qml-module-qtgraphicaleffects mauikit-dev libkf5syntaxhighlighting-dev
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
+	libkf5coreaddons-dev \
+	libkf5i18n-dev \
+	libkf5kio-dev \
+	mauikit-dev \
+	qtbase5-dev \
+	qtdeclarative5-dev
 
-mk-build-deps -i -t "apt-get --yes" -r
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --only-upgrade \
+	extra-cmake-modules
 
-### Clone repo.
+### Clone Repository
 
 git clone --depth 1 --branch v2.1 https://invent.kde.org/maui/mauikit-texteditor.git
 
-mv mauikit-texteditor/* .
+rm -rf mauikit-texteditor/{examples,LICENSE,README.md}
 
-rm -rf mauikit-texteditor examples LICENSES README.md
+### Compile Source
 
-### Build Deb
+mkdir -p mauikit-texteditor/build && cd mauikit-texteditor/build
 
-mkdir source
-mv ./* source/ # Hack for debuild
-cd source
-debuild -b -uc -us
+cmake \
+	-DCMAKE_INSTALL_PREFIX=/usr \
+	-DENABLE_BSYMBOLICFUNCTIONS=OFF \
+	-DQUICK_COMPILER=ON \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_SYSCONFDIR=/etc \
+	-DCMAKE_INSTALL_LOCALSTATEDIR=/var \
+	-DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
+	-DCMAKE_VERBOSE_MAKEFILE=ON \
+	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ..
+
+make
+
+### Run checkinstall and Build Debian Package
+### DO NOT USE debuild, screw it
+
+>> description-pak printf "%s\n" \
+	'A free and modular front-end framework for developing user experiences.' \
+	'' \
+	'MauiKit Text Editor components.' \
+	'' \
+	'Maui stands for Multi-Adaptable User Interface and allows ' \
+	'any Maui app to run on various platforms + devices,' \
+	'like Linux Desktop and Phones, Android, or Windows.' \
+	'' \
+	'This package contains the MauiKit texteditor shared library, the MauiKit texteditor qml module' \
+	'and the MauiKit texteditor development files.' \
+	'' \
+	''
+
+checkinstall -D -y \
+	--install=no \
+	--fstrans=yes \
+	--pkgname=mauikit-texteditor \
+	--pkgversion=2.1.0 \
+	--pkgarch=amd64 \
+	--pkgrelease="1" \
+	--pkglicense=LGPL-3 \
+	--pkggroup=lib \
+	--pkgsource=mauikit-texteditor \
+	--pakdir=../.. \
+	--maintainer="Uri Herrera <uri_herrera@nxos.org>" \
+	--provides=libmauikittexteditor1,libmauikittexteditor-dev,qml-module-org-mauikit-texteditor \
+	--requires="libc6,libgcc-s1,libkf5i18n5,libkf5syntaxhighlighting5,libmauikit \(\>= 2.1.0\),libqt5core5a,libqt5gui5,libqt5qml5,libqt5quick5,libstdc++6,qml-module-org-kde-kirigami2,qml-module-org-kde-mauikit \(\>= 2.1.0\),qml-module-qt-labs-settings,qml-module-qtquick-controls2,qml-module-qtquick-shapes" \
+	--nodoc \
+	--strip=no \
+	--stripso=yes \
+	--reset-uids=yes \
+	--deldesc=yes

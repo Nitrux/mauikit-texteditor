56289e10765bd408514ab7cd98b4a85a79578025
Uri Herrera <hello@uriherrera.com>
2021-11-12 02:43:09 -0600

    fix email

--- travis-build.sh
+++ travis-build.sh
@@ -102,7 +102,7 @@ checkinstall -D -y \
 	--pkggroup=lib \
 	--pkgsource=mauikit-texteditor \
 	--pakdir=../.. \
-	--maintainer="Uri Herrera <uri_herrera@nxos.org>" \
+	--maintainer=uri_herrera@nxos.org \
 	--provides=mauikit-texteditor \
 	--requires="libc6,libgcc-s1,libkf5i18n5,libkf5syntaxhighlighting5,mauikit \(\>= 2.1.0\),libqt5core5a,libqt5gui5,libqt5qml5,libqt5quick5,libstdc++6,qml-module-org-kde-kirigami2,qml-module-qt-labs-settings,qml-module-qtquick-controls2,qml-module-qtquick-shapes" \
 	--nodoc \

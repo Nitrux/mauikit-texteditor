220711dcdf5c2c0746c900c92298ff427f33979c
Uri Herrera <hello@uriherrera.com>
2026-05-12 18:18:42 -0600

    update clone path

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -34,7 +34,7 @@ cmake \
 	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
 	-DCMAKE_VERBOSE_MAKEFILE=ON \
 	-DCMAKE_INSTALL_LIBDIR="/usr/lib/${HOST_MULTIARCH}" \
-	../mauikit-texteditor/
+	../mauikit-texteditor-src/
 
 make -j"$(nproc)"
 
@@ -66,7 +66,7 @@ checkinstall -D -y \
 	--pkgrelease="1" \
 	--pkglicense=LGPL-3 \
 	--pkggroup=libs \
-	--pkgsource=mauikit-texteditor \
+	--pkgsource=mauikit-texteditor-src \
 	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=mauikit-texteditor \
